7d808f8a895269691274c0a2b5d37eb2ffdfd757
aditiya-amaranggana <aditiya.amaranggana@jago.com>
2021-09-13 15:28:01 +0700

    Update Data Pelanggan
    Link menggunakan external

--- controller/pelanggan_controller.php
+++ controller/pelanggan_controller.php
@@ -16,7 +16,7 @@ function function_konveksi_pelanggan() {
 
 	$user = wp_get_current_user();
 
-	echo "<link rel='stylesheet' href='".plugin_dir_url( dirname( __FILE__ ) ) . "assets/css/bootstrap.css' media='all' />";
+	echo '<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">';
 	echo "<link rel='stylesheet' href='".plugin_dir_url( dirname( __FILE__ ) ) . "assets/css/select2.min.css' media='all' />";
 
 	echo "<script src='https://code.jquery.com/jquery-3.5.1.slim.min.js' integrity='sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj' crossorigin='anonymous'></script>";
