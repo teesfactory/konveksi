48b51e7c9adc9a829091cb2736d11097c7fd45d6
teesfactory <teesfactory.co@gmail.com>
2021-11-01 20:23:00 +0700

    Update route.php

--- route.php
+++ route.php
@@ -38,6 +38,10 @@ function function_konveksi_konfigurasi() {
 	echo "<h2>Test Konveksi Apps</h2>";
 }
 
+function function_konveksi_master() {
+	echo "<h2>Test Konveksi Apps</h2>";
+}
+
 function konveksi_form_simulasi_harga(){
     return 'Follow us on <a rel="nofollow" href="https://www.facebook.com/ThemeXpert/">Facebook</a>';
 }
